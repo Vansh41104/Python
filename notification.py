1b4deb80ffa10e15890417533bf245154114c52b
Vansh Bhatnagar <115162664+Vansh41104@users.noreply.github.com>
2023-10-09 13:43:13 +0530

    Add files via upload

---
+++ notification.py
@@ -0,0 +1,17 @@
+import time
+from plyer import notification
+
+
+if _name=="__main_":
+
+		notification.notify(
+			title = "HEY !!!!!!!",
+			message="DRINK WATER" ,
+		
+		
+			# displaying time
+			timeout=2
+)
+		# waiting time
+		time.sleep(5)
+		
